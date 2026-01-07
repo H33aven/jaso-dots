46d59a1aca78310ca8870185b110b023698ddfac
Eto Disappointed <192628995+H33aven@users.noreply.github.com>
2026-01-07 05:57:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,13 +9,14 @@ echo -e "${GREEN}[+]${NC} Creating directories..."
 mkdir -p ~/.config/hypr/hyprland || { echo -e "${RED}[-] Failed to create hyprland directory${NC}"; exit 1; }
 mkdir -p ~/.config/illogical-impulse || { echo -e "${RED}[-] Failed to create illogical-impulse directory${NC}"; exit 1; }
 mkdir -p ~/.config/neofetch || { echo -e "${RED}[-] Failed to create neofetch directory${NC}"; exit 1; }
+mkdir -p ~/.config/vesktop || { echo -e "${RED}[-] Failed to create vesktop directory${NC}"; exit 1; }
 
 echo -e "${GREEN}[+]${NC} Deploying configs..."
 cp -f cfgs/hypr/hyprland/general.conf ~/.config/hypr/hyprland/ && echo -e "${GREEN}[+] general.conf deployed${NC}" || echo -e "${RED}[-] Failed to deploy general.conf${NC}"
 cp -f cfgs/hypr/hyprland/keybinds.conf ~/.config/hypr/hyprland/ && echo -e "${GREEN}[+] keybinds.conf deployed${NC}" || echo -e "${RED}[-] Failed to deploy keybinds.conf${NC}"
 cp -f cfgs/illogical-impulse/config.json ~/.config/illogical-impulse/ && echo -e "${GREEN}[+] config.json deployed${NC}" || echo -e "${RED}[-] Failed to deploy config.json${NC}"
 cp -f cfgs/neofetch/config.conf ~/.config/neofetch/ && echo -e "${GREEN}[+] config.conf deployed${NC}" || echo -e "${RED}[-] Failed to deploy config.conf${NC}"
-
+cp -f themes ~/.config/vesktop && echo -e "${GREEN}[+] config.conf deployed${NC}" || echo -e "${RED}[-] Failed to deploy config.conf${NC}"
 
 hyprctl reload && echo -e "${GREEN}[+] Hyprland reloaded${NC}" || echo -e "${RED}[-] Failed to reload Hyprland${NC}"
 echo -e "${GREEN}[+]${NC} Deployment complete. System configured."
